1c7d0a3784dd651e972ada14f52a1d8dc14f3710
Yaugen <Yaugen.Yaugenavich@yandex.ru>
2020-06-24 22:08:07 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,128 +1,146 @@
- function calc(param) {
-    param = param;
+ function calc (param) {
+    param = param || null;
 
     let add = function () {
 
-        let pickup = document.querySelector('.pickup');
-        pickup.classList.add('add');
+        let pickup = document.querySelector ('.pickup');
+        pickup.classList.add ('add');
 
-        pickup.addEventListener('click', function () {
-            let wiev = document.querySelector('.wiev');
-            wiev.classList.add('add');
+        pickup.addEventListener ('click', function () {
+            let wiev = document.querySelector ('.wiev');
+            wiev.classList.add ('add');
         });
-        let close = document.querySelector('.app .wiev .close');
-        close.addEventListener('click', function() {
-            let wievClose = document.querySelector('.app .wiev');
+        let close = document.querySelector ('.app .wiev .close');
+        close.addEventListener ('click', function () {
+            let wievClose = document.querySelector ('.app .wiev');
            
-            wievClose.classList.remove('add');
-            pickup.classList.remove('add');
+            wievClose.classList.remove ('add');
+            pickup.classList.remove ('add');
+            window.location.reload ();
         });
-        init();
+
+        init ();
     };
-    setTimeout(add, 1000);
+    setTimeout (add, 1100);
 
     let init = function () {
-        let pay = document.querySelector('.pay');
-        pay.addEventListener('click', function () {
+        let pay = document.querySelector ('.pay');
+        pay.addEventListener ('click', function () {
 
             let draft = function (param) {
-                param = param;
+                param = param || null;
 
-                let number = document.querySelectorAll('.app .wiev .draft input[type="number"]'),
-                    radio = document.querySelectorAll('.app .wiev .draft input[type="radio"]'),
+                let number = document.querySelectorAll ('.app .wiev .draft input[type="number"]'),
+                    radio = document.querySelectorAll ('.app .wiev .draft input[type="radio"]'),
                     radioValue = '',
-                    resDraft;
+                    resDraft ;
 
-                radio.forEach(function (element) {
+                radio.forEach (function (element) {
                     if (element.checked) radioValue = element.value;
                 });
-                let numberValue = Object.values(number);
-                resDraft = Math.round(((numberValue[0].value * radioValue) + (numberValue[1].value * radioValue) + (numberValue[2].value) * radioValue) * param);
+                let numberValue = Object.values (number);
+                resDraft = Math.round (((numberValue[0].value * radioValue) + (numberValue[1].value * radioValue) + (numberValue[2].value) * radioValue) * param);
 
                 return resDraft;
 
             };
 
             let street = function (param) {
-                param = param;
+                param = param || null;
 
-                let number = document.querySelectorAll('.app .wiev .street input[type="number"]'),
-                    radio = document.querySelectorAll('.app .wiev .street input[type="radio"]'),
+                let number = document.querySelectorAll ('.app .wiev .street input[type="number"]'),
+                    radio = document.querySelectorAll ('.app .wiev .street input[type="radio"]'),
                     radioValue = '',
-                    resStreet;
+                    resStreet ;
 
-                radio.forEach(function (element) {
+                radio.forEach (function (element) {
                     if (element.checked) radioValue = element.value;
                 });
                 let numberValue = Object.values(number);
-                resStreet = Math.round((numberValue[0].value * radioValue) * param);
+                resStreet = Math.round ((numberValue[0].value * radioValue) * param);
 
                 return resStreet;
             };
 
             let clean = function (param) {
-                param = param;
+                param = param || null;
                 const powerCabel = 6 ,
                       cabel = 3 ,
                       pribor = 7 ,
                       camera = 5 ;  
 
-                let number = document.querySelectorAll('.app .wiev .clean input[type="number"]'),
+                let number = document.querySelectorAll ('.app .wiev .clean input[type="number"]'),
                     resClean,
                     numberValue = Object.values(number);
 
-                resClean = Math.round(((numberValue[0].value * cabel) + (numberValue[1].value * powerCabel) + (numberValue[2].value * cabel) + (numberValue[3].value * cabel) + (numberValue[4].value * pribor) + (numberValue[5].value * camera)) * param);
+                resClean = Math.round ( ((numberValue[0].value * cabel) + (numberValue[1].value * powerCabel) + (numberValue[2].value * cabel) + (numberValue[3].value * cabel) + (numberValue[4].value * pribor) + (numberValue[5].value * camera)) * param );
 
                 return resClean;
             };
 
             let money = function () {
              
-                let checkbox = document.querySelectorAll('.app .wiev ul li input[type="checkbox"]'),
-                    checkboxElem;
-                checkbox.forEach(function (element) {
+                let checkbox = document.querySelectorAll ('.app .wiev ul li input[type="checkbox"]'),
+                    checkboxElem ;
+
+                    checkbox.forEach (function (element) {
                     if (element.checked) checkboxElem = element.value;
+                   // console.log(checkboxElem);
                 });
 
                 return checkboxElem;
             };
 
             let showInit = function (param) {
-                param = param;
+                param = param || null;
 
-                let divWraper = document.createElement('div'),
-                    divElem1 = document.createElement('div'),
-                    divElem2 = document.createElement('div'),
-                    divElem3 = document.createElement('div');
+                let divWraper = document.createElement ('div'),
+                    divElem1 = document.createElement ('div'),
+                    divElem2 = document.createElement ('div'),
+                    divElem3 = document.createElement ('div'),
+                    buttonElem = document.createElement ('button');
 
-                divElem1.innerHTML = param[0];
-                divElem2.innerHTML = param[1];
-                divElem3.innerHTML = param[2];
+                    buttonElem.style = `position: absolute;
+                                        top: 10px;
+                                        right: 10px;
+                                        color: #fff;
+                                        border: 2px solid #fff;
+                                        border-radius: 3px;
+                                        background-color: blue;
+                                        width: 20px;
+                                        hight: 10px;`     
 
-                divWraper.appendChild(divElem1);
-                divWraper.appendChild(divElem2);
-                divWraper.appendChild(divElem3);
+                    divElem1.innerHTML = 'черновые работы' + ' ' + param[0];
+                    divElem2.innerHTML = 'ввод питания' + ' ' + param[1];
+                    divElem3.innerHTML = 'чистовые работы' + ' ' + param[2];
+                    buttonElem.innerHTML = 'x';
 
-                divWraper.classList.add('resultat');
+                    divWraper.appendChild (divElem1);
+                    divWraper.appendChild (divElem2);
+                    divWraper.appendChild (divElem3);
+                    divWraper.appendChild (buttonElem);
 
-                let site = document.querySelector('.site');
-                site.appendChild(divWraper);
+                    divWraper.classList.add ('resultat');
+
+                let site = document.querySelector ('.site');
+                site.appendChild (divWraper);
 
                 let del = function(){ 
-                    let delElem  = document.querySelector('.site .resultat');  
-                    delElem.remove(delElem);
+                    let delElem = document.querySelector ('.site .resultat'), 
+                        delButtonElem = document.querySelector ('.site .resultat');  
+                        delButtonElem.remove (delElem);
                 }
                 
-                divWraper.addEventListener('click', del);
+                divWraper.addEventListener ('click', del);
                     
             };
-            showInit([draft(money()), street(money()), clean(money())]);
+            showInit ( [draft(money()), street(money()), clean(money())] );
         });
 
     };
 
 };
 
-window.addEventListener('load', function () {
-    calc('app1');
+window.addEventListener ('load', function () {
+    calc ('app1');
 });
